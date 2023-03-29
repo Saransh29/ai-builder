96aa3acba3568a435b26a9f94c438c445e79144e
Saransh29 <saranshbibiyan234@gmail.com>
2023-03-29 21:28:44 +0530

    added some more meta stuff

--- app/layout.js
+++ app/layout.js
@@ -7,31 +7,39 @@ export const metadata = {
   title: "Ai Builder",
   description:
     "Generate full websites with Ai, in less than 1 minute. Poweed by ChatGPT",
-  opengraph: {
-    title: "Ai Builder",
-    description: "Generate full websites with Ai.",
-    url: "https://ai-builder.vercel.app/",
-    siteName: "Ai Builder",
-    images: [
-      {
-        url: "https://ai-builder-gules.vercel.app/og-svg.svg",
-        width: 800,
-        height: 400,
-        alt: "Ai Builder",
-      },
-    ],
-    locale: "en-US",
-    type: "website",
-  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <meta property="og:url" content="https://ai-builder-gules.vercel.app/" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Ai Builder" />
+      <meta
+        property="og:description"
+        content="Generate full websites with Ai, in less than 1 minute. Poweed by ChatGPT"
+      />
       <meta
         property="og:image"
         content="https://ai-builder-gules.vercel.app/og-svg.svg"
       />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="ai-builder-gules.vercel.app" />
+      <meta
+        property="twitter:url"
+        content="https://ai-builder-gules.vercel.app/"
+      />
+      <meta name="twitter:title" content="Ai Builder" />
+      <meta
+        name="twitter:description"
+        content="Generate full websites with Ai, in less than 1 minute. Poweed by ChatGPT"
+      />
+      <meta
+        name="twitter:image"
+        content="https://ai-builder-gules.vercel.app/og-svg.svg"
+      />
+
       <Script
         strategy="afterInteractive"
         async
