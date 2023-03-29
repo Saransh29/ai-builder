a20ced38ec80391c030e50644332db74bed0a0e2
Saransh29 <saranshbibiyan234@gmail.com>
2023-03-29 21:16:02 +0530

    fixed meta:og-image

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,8 @@ import Script from "next/script";
 
 export const metadata = {
   title: "Ai Builder",
-  description: "Generate full websites with Ai.",
+  description:
+    "Generate full websites with Ai, in less than 1 minute. Poweed by ChatGPT",
   opengraph: {
     title: "Ai Builder",
     description: "Generate full websites with Ai.",
@@ -19,12 +20,18 @@ export const metadata = {
         alt: "Ai Builder",
       },
     ],
+    locale: "en-US",
+    type: "website",
   },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <meta
+        property="og:image"
+        content="https://ai-builder-gules.vercel.app/og-svg.svg"
+      />
       <Script
         strategy="afterInteractive"
         async
