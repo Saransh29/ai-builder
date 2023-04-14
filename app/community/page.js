c55abd180e56c366dd88177476ac0858e877c409
Saransh29 <saranshbibiyan234@gmail.com>
2023-04-15 02:00:49 +0530

    fixed the tailwind config file and clip-rule error

--- app/community/page.js
+++ app/community/page.js
@@ -112,7 +112,7 @@ export default function About() {
                 <path
                   fill-rule="evenodd"
                   d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 ></path>
               </svg>
               Prev
@@ -132,7 +132,7 @@ export default function About() {
                 <path
                   fill-rule="evenodd"
                   d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 ></path>
               </svg>
             </button>
